0e787a423d70300e757a573bda06cd65da7d85aa
andrej <a>
2020-03-18 15:31:33 +0100

    .

--- kubernetes-conf/devel/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t iipsrv-builder .
-if [ -n "${KAKADU_HOME+1}" ]; then
-  s2i build --incremental=false --rm --ref=master --inject $KAKADU_HOME:/tmp/kakadu git@github.com:moravianlibrary/iipsrv.git iipsrv-builder iipsrv-devel
-else
-  s2i build --incremental=false --rm --ref=master https://github.com/moravianlibrary/iipsrv-openjpeg.git iipsrv-builder iipsrv-devel
-fi
